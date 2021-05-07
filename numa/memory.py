1951fecc05d39bfbd9f8cb955b36cf74615684f6
lu-z18 <lu-z18@mails.tsinghua.edu.cn>
2021-05-07 19:24:14 +0800

    memory

--- numa/memory.py
+++ numa/memory.py
@@ -1,40 +1,83 @@
 from numa import LIBNUMA
 from typing import List, Optional
-from ctypes import c_longlong
+from ctypes import c_longlong, c_int
+import numa.utils as numa_utils
 
 __all__ = ["get_allocation_allowed_nodes", "set_interleave_nodes", "set_local_alloc", "set_membind_nodes",
            "get_membind_nodes", "get_interleave_nodes", "set_membind_policy"]
 
 
 def get_interleave_nodes() -> List[int]:
-    return []
+    result_nodes_pointer = LIBNUMA.get_interleave_nodes()
+    try:
+        result_nodes_pointer.contents
+    except ValueError:
+        raise Exception(f"get interleave nodes info failed")
+    return numa_utils.get_bitset_list(result_nodes_pointer)
 
 
 def set_interleave_nodes(*nodes) -> None:
-    pass
+    nodes = list(set(nodes))
+    res = ",".join(list(map(str, nodes)))
+    c_string = bytes(res, "ascii")
+    bitmask = LIBNUMA.numa_parse_nodestring(c_string)
+    op_res = LIBNUMA.numa_set_interleave_mask(bitmask)
+    if op_res == -1:
+        raise Exception(f"set interleave_nodes {res} failed")
+
 
 
 def set_local_alloc(strict_policy: Optional[bool] = False) -> None:
-    pass
+    set_membind_policy(strict_policy)
+    LIBNUMA.set_local_alloc()
 
 
 def set_membind_nodes(strict_policy: Optional[bool] = False, *nodes) -> None:
-    pass
+    set_membind_policy(strict_policy)
+    nodes = list(set(nodes))
+    res = ",".join(list(map(str, nodes)))
+    c_string = bytes(res, "ascii")
+    bitmask = LIBNUMA.numa_parse_nodestring(c_string)
+    op_res = LIBNUMA.numa_set_membind(bitmask)
+    if op_res == -1:
+        raise Exception(f"set membind nodes {res} failed")
 
 
-def get_membind_nodes(*nodes) -> List[int]:
-    return []
+def get_membind_nodes() -> List[int]:
+    result_nodes_pointer = LIBNUMA.numa_get_membind()
+    try:
+        result_nodes_pointer.contents
+    except ValueError:
+        raise Exception(f"get membind nodes info failed")
+    return numa_utils.get_bitset_list(result_nodes_pointer)
 
 
 def set_membind_policy(strict_policy: Optional[bool] = False) -> None:
-    pass
+    strict_policy = c_int()
+    LIBNUMA.numa_set_bind_policy(strict_policy)
 
 
 def get_allocation_allowed_nodes() -> List[int]:
-    return []
+    result_nodes_pointer = LIBNUMA.get_allocation_allowed_nodes()
+    try:
+        result_nodes_pointer.contents
+    except ValueError:
+        raise Exception(f"get allocation allowed nodes info failed")
+    return numa_utils.get_bitset_list(result_nodes_pointer)
 
 
 def node_memory_info(node: int) -> tuple:
     free_size = c_longlong()
     total_size = LIBNUMA.numa_node_size64(node, free_size)
     return total_size, free_size.value
+
+
+def set_membind_balancing(strict_policy: Optional[bool] = False, *nodes) -> List[int]:
+    set_membind_policy(strict_policy)
+    nodes = list(set(nodes))
+    res = ",".join(list(map(str, nodes)))
+    c_string = bytes(res, "ascii")
+    bitmask = LIBNUMA.numa_parse_nodestring(c_string)
+    op_res = LIBNUMA.numa_set_membind_balancing(bitmask)
+    if op_res == -1:
+        raise Exception(f"set membind balancing nodes {res} failed")
