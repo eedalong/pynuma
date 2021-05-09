83c752377b16c1410fe07ba27b6aba20714ae386
eedalong <ee_dalong@163.com>
2021-05-09 14:52:37 +0800

    fix version

--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
 from setuptools import setup
 
+
 def read(fname):
     with open(fname, 'r') as f:
         return f.read()
 
-setup(name             = 'py-libnuma',
+
+setup(
+      name             = 'py-libnuma',
       description      = 'Python3 libnuma ctypes wrapper',
       long_description = read('README.md'),
       long_description_content_type="text/markdown",
       py_modules       = ['numa'],
-      version          = '1.0',
+      version          = '1.1',
       author           = 'Xiulong Yuan, Zhan Lu, Zheng Zeng',
       author_email     = 'yuanxl19@mails.tsinghua.edu.cn, lu-z18@mails.tsinghua.edu.cn, zengz17@mails.tsinghua.edu.cn',
       url              = 'https://github.com/eedalong/pynuma',
